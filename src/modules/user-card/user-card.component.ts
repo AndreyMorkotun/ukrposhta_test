ccc2486deba9f9694380fb7001ee0848d546deb6
Andrii Morkotun <peaceful3478@gmail.com>
2025-12-23 01:10:46 +0200

    Added expansion panel to the user card

--- src/modules/user-card/user-card.component.ts
+++ src/modules/user-card/user-card.component.ts
@@ -3,12 +3,13 @@ import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
 import { User } from '../../core/interfaces/user';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';import { MatListModule } from '@angular/material/list';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-user-card',
   templateUrl: './user-card.component.html',
   styleUrl: './user-card.component.css',
-  imports: [MatButtonModule, MatDialogModule, MatIconModule, MatListModule]
+  imports: [MatButtonModule, MatDialogModule, MatIconModule, MatListModule, MatExpansionModule]
 })
 export class UserCardComponent {
 constructor(@Inject(MAT_DIALOG_DATA) public data: User) {}
